c4935981aac85d25afde552689dfdab4afec51b4
David Rios <drios@gather.town>
2026-01-28 13:03:18 -0500

    chore: added comment

--- src/types/payload.ts
+++ src/types/payload.ts
@@ -2,6 +2,8 @@ export interface QueryError {
     page: -1,
     resultCount: -1,
     // Handles both custom and axios errors from server
+    // The axios type is intentionally partial: errors include more than just a 'message' field, but
+    // no more detailed information is required at the moment
     error: string | { message: string }
 }
 
